bfe772592440bfdf5de8305ca5481dfe8838187d
Jenny Tan <tanjenny.yo@gmail.com>
2020-09-29 23:40:54 +0000

    initial working function

---
+++ game.py
@@ -0,0 +1,30 @@
+"""A number-guessing game."""
+
+# Put your code here
+import random
+#greet player
+print("Hi There")
+#get player name
+name = input('What is your name? ')
+#choose random number between 1 and 100
+correct_num = random.randint(1,100)
+#repeat forever:
+    # get guess
+    # if guess is incorrect:
+    #     give hint
+    #     increase number of guesses
+    # else:
+    #     congratulate player
+guesses = 1
+
+while True: 
+    guess = int(input('What number would you like to guess? '))
+    if guess == correct_num:
+        print (f'Congratulations! You guessed it in {guesses} tries')
+        break
+    elif guess < correct_num: 
+        print('too low, try again')
+        guesses += 1
+    elif guess > correct_num:
+        print('too high, try again')
+        guesses += 1
